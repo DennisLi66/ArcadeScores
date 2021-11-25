7b0b38ba6e896a906778f6bfe4fd5b12c999cd2b
Dennis Li <46885316+DennisLi66@users.noreply.github.com>
2021-11-24 19:12:06 -0800

    Create dbCreationScripts.sql

---
+++ mysql/dbCreationScripts.sql
@@ -0,0 +1,22 @@
+drop database if exists arcadeScoresDB;
+create database arcadeScoresDB;
+use arcadeScoresDB;
+
+create table users (
+	userID int primary key auto_increment,
+    username varchar(50),
+    email varchar(255) NOT NULL UNIQUE,
+    passcode varchar(50)
+);
+create table times (
+	timeScoreID int primary key auto_increment,
+	userID int,
+    timeInMilliseconds int,
+    submissionTime datetime
+);
+create table scores (
+	scoreID int primary key auto_increment,
+    userID int,
+    score int,
+    submissionTime datetime
+);
