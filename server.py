c5b3372f2caf3ed179bf7172eb1c3e427283e2fd
Dennis Li <46885316+DennisLi66@users.noreply.github.com>
2021-11-26 10:33:45 -0800

    Added checking if session is valid

--- server.py
+++ server.py
@@ -24,11 +24,34 @@ class Login(Resource):
         return;
 
 class Session(Resource):
-    def get(self): #Check Session Valid
-        return;
+    def post(self): #Check Session Valid
+        parser = reqparse.RequestParser();
+        parser.add_argument('userID',required=True);
+        parser.add_argument('sessionID',required=True);
+        args = parser.parse_args();
+        try:
+            connection = formConnection();
+            query = """
+            SELECT * FROM sessions
+            RIGHT JOIN
+            (select userID,max(sessionDate) as high from sessions group by userID) highDates
+            ON highDates.userID = sessions.userID
+            WHERE userID = %s AND sessionID = %s
+            AND ((timeDuration = 'forever')
+            OR (timeduration = "HOUR" AND NOW() < date_add(sessionDate,Interval 1 Hour)))
+            """;
+            cursor = connection.cursor(prepared=True);
+            cursor.execute(query,(args['userID'],args['sessionID']));
+            connection.commit();
+            results = cursor.fetchall();
+            connection.close();
+            print(results);
+            return results;
+        except:
+            raise ValueError('Querying Failed');
     def patch(self): #Update Session
         return;
-    def delete(self): #Delete Session
+    def delete(self): #Expire Session 
         return; 
 
 class Scores(Resource):
