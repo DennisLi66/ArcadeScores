5441a3d648954b3a653b8025fe8b53d0efb1a02b
Dennis Li <46885316+DennisLi66@users.noreply.github.com>
2021-11-30 23:45:13 -0800

    Update loginEndpoint.py
    Completed but Untested

--- loginEndpoint.py
+++ loginEndpoint.py
@@ -11,21 +11,41 @@ class Login(Resource):
         parser = reqparse.RequestParser();
         parser.add_argument('email',required=True);
         parser.add_argument('password',required=True);
+        parser.add_argument('timeDuration',required=True);
         args = parser.parse_args();
         try:
             connection = formConnection();
-            query = "SELECT * FROM users WHERE email = %s;"
             cursor = connection.cursor(prepared = True);
-            cursor.execute(query,(args['email']));
-            connection.commit();
+            cursor.execute("SELECT * FROM users WHERE email = %s;",[args['email']]);
+            #connection.commit();
             results = cursor.fetchall();
-            if (results.length == 0):
+            if (len(results) == 0):
                 connection.close();
                 return {'status':-1,'message':'No Such Account.'}
             else:
                 print(results);
-                connection.close();
-                return results;
+                hashPass = "";
+                salt = "";
+                userID = ""; #Might Be Int //CHECK THIS
+                for row in records:
+                    salt = row[3];
+                    userID = row[0];
+                    #hashPass = row[4];
+                #check password: if correct create sessionID and send back as result
+                    #else go away
+                #newHashPass = bcrypt.hashpw(args['password'],salt)
+                if bcrypt.checkpw(args['password'],hashPass):
+                    sessionSequence = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
+                    insertSessionQuery = """
+                    INSERT INTO sessions (sessionID,userID,sessionDate,timeDuration) VALUES (%s,%s,NOW(),%s)
+                    """
+                    cursor = connection.cursor(prepared = True);
+                    cursor.execute(insertSessionQuery,[sessionSequence,userID,args['timeDuration']])
+                    connection.close();
+                    return {'status': 0, 'sessionID':sessionSequence};
+                else:
+                    connection.close();
+                    return {'status': -1, 'message':'No Matches'}, 200
             
         except:
             raise ValueError('Querying Failed.'); 
