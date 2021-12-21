6681b684f15a393dd78c42bd6f30ff414d4fcd85
Dennis Li <46885316+DennisLi66@users.noreply.github.com>
2021-12-20 23:41:46 -0800

    added forgotpasswordcode endpoint
    moved code from changePassword to forgotpassword

--- forgotPasswordCodeEndpoint.py
+++ forgotPasswordCodeEndpoint.py
@@ -5,7 +5,26 @@ from formConnectionModule import formConnection
 
 class ForgotPasswordCode(Resource):
     def post(self):
-        print();
+        parser = reqparser.RequestParser();
+        parser.add_argument('code',required=True);
+        parser.add_argument('email',required=True);
+        args = parser.parse_arg();
+        try:
+            connection = formConnection();
+            query = """
+            SELECT * FROM codes WHERE email = %s AND code = %s
+            """
+            cursor = connection.cursor(prepared = True);
+            cursor.execute(query,(args['email'],args['code']));
+            res = cursor.fetchall();
+            connection.commit();
+            connection.close();
+            if not res:
+                return {'status':-2,"message":"Bad Match"}
+            else:
+                return {'status':0,"message": "Match"}        
+        except Exception as e:
+            return {'message': str(e), 'status': -1}; 
         
 if __name__ == '__main__':
     print("Forgot Password Code Endpoint...");
--- forgotPasswordEndpoint.py
+++ forgotPasswordEndpoint.py
@@ -5,7 +5,26 @@ from formConnectionModule import formConnection
 
 class ForgotPassword(Resource):
     def post(self):
-        print();
+        parser = reqparse.RequestParser();
+        parser.add_argument('email',required=True);
+        args = parser.parse_args();
+        try:
+            letters = string.digits + string.ascii_uppercase;
+            code = ''.join(random.choice(letters) for i in range(10));
+            connection = formConnection();
+            #randomly generate a code
+            query = """
+            INSERT INTO forgottenPasswordCodes (email,fcode)
+            SELECT email,%s FROM users WHERE email = %s
+            ON DUPLICATE KEY UPDATE fcode = %s
+            """
+            cursor = connection.cursor(prepared=True);
+            cursor.execute(query,(code,args['email'],code));
+            connection.commit();
+            connection.close();
+            return {'status':0};
+        except Exception as e:
+            return {'message': str(e), 'status': -1}; 
         
 if __name__ == '__main__':
     print("Forgot Password Endpoint...");
